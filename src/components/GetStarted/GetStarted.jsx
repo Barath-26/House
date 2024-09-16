0cf15e0ccf97eab5cc015e9c38aa5374fbf38089
Barath-26 <sbarath26112004@gmail.com>
2024-09-16 14:04:20 +0530

    first

---
+++ src/components/GetStarted/GetStarted.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./GetStarted.css";
+const GetStarted = () => {
+  return (
+    <div id="get-started" className="g-wrapper">
+    </div>
+  );
+};
+
+export default GetStarted;
